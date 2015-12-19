818b8d8a550bc614987a58a7e72c4409eb803f6c
= <joneubank@gmail.com>
2015-12-19 16:13:59 -0500

    Day 12 complete

---
+++ d12p2.js
@@ -0,0 +1,56 @@
+/*
+Advent of Code
+Day 12
+Problem 2
+
+Input: d012i.json
+*/
+
+(function()
+{
+'use strict';
+
+    var total = 0;
+
+    var redCheck = function(obj)
+    {
+        var keys = Object.keys(obj);
+        for(var i = 0; i < keys.length; i++)
+        {
+            if(obj[keys[i]] === 'red'){
+                return true;
+            }
+        }
+        return false;
+    };
+
+    var recursiveGetNums = function(obj)
+    {
+        if(Array.isArray(obj) || !redCheck(obj))
+        {
+            var keys = Object.keys(obj);
+            for(var i = 0; i < keys.length; i++)
+            {
+                var item = obj[keys[i]];
+                if(typeof(item) === 'object')
+                {
+                    // console.log("Object Found: " + item);
+                    recursiveGetNums(item);
+                } else if(typeof(item) === 'number')
+                {
+                    console.log("Number Found: " + item);
+                    total += parseInt(item);
+                }
+            }
+        }
+    };
+
+
+    /* ===============
+    *   Read Input and Main Logic
+    *  =============== */
+    var fs = require('fs');
+    var obj = JSON.parse(fs.readFileSync('d12i.json', 'utf8'));
+    recursiveGetNums(obj);
+    console.log(total);
+})();
