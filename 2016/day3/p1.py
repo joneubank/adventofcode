25eb3024bdef7e6025b6a29f8059d025e0ce635e
= <joneubank@gmail.com>
2016-12-03 12:18:29 -0500

    Day 3 setup

---
+++ 2016/day3/p1.py
@@ -0,0 +1,20 @@
+"""
+Advent of Code
+Day : 3
+Problem : 1
+"""
+from everyday import utils
+
+# Since this isn't absolute path, you need to run this file from within this directory
+INPUT_FILE = 'input.txt'
+
+
+def main():
+    input_lines = utils.get_input_lines(INPUT_FILE)
+
+
+    for index, line in enumerate(input_lines):
+        pass
+
+if __name__ == '__main__':
+    main()
