bfc6b3c66dac88f8cd47a2daa84a73cccb235415
= <joneubank@gmail.com>
2016-12-04 10:58:21 -0500

    Day4 Initial Setup

---
+++ 2016/day4/p2.py
@@ -0,0 +1,21 @@
+"""
+Advent of Code
+Day : 4
+Problem : 2
+"""
+from everyday import utils
+
+# Since this isn't absolute path, you need to run this file from within this directory
+INPUT_FILE = 'ilana-input.txt'
+
+
+def main():
+    input_lines = utils.get_input_lines(INPUT_FILE)
+
+    for index, line in enumerate(input_lines):
+
+        print("{}: {}".format(index, line, valid))
+
+
+if __name__ == '__main__':
+    main()
